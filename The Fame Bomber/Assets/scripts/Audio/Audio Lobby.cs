458735fd09d5eb3c6525b5bce808833dab673da4
Levos22 <154330850+Levos22@users.noreply.github.com>
2024-03-18 16:08:41 +0100

    Audio lobby amélioration

--- The Fame Bomber/Assets/scripts/Audio/Audio Lobby.cs
+++ The Fame Bomber/Assets/scripts/Audio/Audio Lobby.cs
@@ -7,27 +7,23 @@ using UnityEngine;
 [System.Serializable]
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource audioSource;
+    public AudioSource music;
+    public AudioClip Musique;
     [Range(0f, 1f)]
     public float volume;
 
     // Start is called before the first frame update 
     void Start()
     {
-        audioSource.volume = volume;
-        PlayMusic("country-dreams-country-ballad-621");
-    }
-    public void PlayMusic(string name)
-    {
-        AudioClip clipToPlay = Resources.Load<AudioClip>("Audio/" + name);
-        audioSource.clip = clipToPlay;
-        audioSource.Play();
+        music.volume = volume;
+        music.clip = Musique;
+        music.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        audioSource.volume = volume;
+        music.volume = volume;
     }
 }
 
