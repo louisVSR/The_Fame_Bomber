8ff7c2df314a7f314d12a42d22a0cf5d4fe53bbe
JohanneCardon <154500188+JohanneCardon@users.noreply.github.com>
2024-03-18 15:39:49 +0100

    IAAméliorationEtModifIntro
    Rotation de l'IA
    Et modif intro

--- The Fame Bomber/Assets/scripts/IACuisinier.cs
+++ The Fame Bomber/Assets/scripts/IACuisinier.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,12 +14,13 @@ public class IACuisinier : MonoBehaviour
     private int etat;
     private const int MOUVEMENT = 0;
     private const int ATTENTE = 1;
+    private const int SETOURNERVERS = 2;
     private float compteur;
     private float aAttendre;
+    private Quaternion pos;
 
     public TextMeshPro text;
 
-    // Start is called before the first frame update
     void Start()
     {
         position = 0;
@@ -28,29 +30,39 @@ public class IACuisinier : MonoBehaviour
         text = GetComponentInChildren<TextMeshPro>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (etat==ATTENTE)
+        if (etat == ATTENTE)
         {
             compteur += Time.deltaTime;
-            if (compteur > aAttendre) 
+            if (compteur > aAttendre)
             {
                 etat = MOUVEMENT;
                 position = (position + 1) % wayPoints.Count;
                 nma.SetDestination(wayPoints[position].transform.position);
             }
-            
+
+        }
+        if (etat == SETOURNERVERS)
+        {
+            compteur++;
+            this.transform.rotation = Quaternion.Lerp(pos, wayPoints[position].transform.rotation, compteur / 30);
+            if (this.transform.rotation == wayPoints[position].transform.rotation)
+            {
+                etat = ATTENTE;
+                compteur = 0;
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if ((other.gameObject.layer == 7) && (nma.remainingDistance<0.5))
+        if ((other.gameObject.layer == 7) && (nma.remainingDistance < 0.5))
         {
-            etat = ATTENTE;
+            etat = SETOURNERVERS;
             compteur = 0;
             aAttendre = other.gameObject.GetComponent<wpControler>().tempsAttente;
+            pos = transform.rotation;
         }
     }
 }
